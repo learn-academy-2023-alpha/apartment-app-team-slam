3d07e1b00daeab8200647d216bcd0eb7e7e12bcc
Aaron Criner <aaroncriner@Aarons-MacBook-Pro.local>
2023-04-12 15:07:09 -0700

    fixed seed functionality and fixed model validation for sqaure_footage (sic)

--- db/seeds.rb
+++ db/seeds.rb
@@ -11,7 +11,7 @@ user1_apartment = [{
     price: 5000,
     utilities: "gas and electric",
     pets: true,
-    sqaure_footage: 200,
+    square_footage: 200,
     image: "https://images.unsplash.com/photo-1522708323590-d24dbb6b0267?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80"
 },
 
@@ -24,7 +24,7 @@ user1_apartment = [{
     price: 5560,
     utilities: "gas and electric",
     pets: true,
-    sqaure_footage: 210,
+    square_footage: 210,
     image: "https://images.unsplash.com/photo-1493809842364-78817add7ffb?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80"
 }
 ] 
@@ -38,7 +38,7 @@ user1_apartment = [{
     price: 5000,
     utilities: "gas and electric",
     pets: true,
-    sqaure_footage: 200,
+    square_footage: 200,
     image: "https://images.unsplash.com/photo-1556020685-ae41abfc9365?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80"
 },
 
@@ -51,11 +51,19 @@ user1_apartment = [{
     price: 5560,
     utilities: "gas and electric",
     pets: true,
-    sqaure_footage: 210,
+    square_footage: 210,
     image: "https://images.unsplash.com/photo-1630699144867-37acec97df5a?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80"
 }
 ] 
 
 
 
-
+user1_apartment.each do |apartment|
+    user1.apartments.create(apartment)
+    p "created: #{apartment}"
+  end
+  
+  user2_apartment.each do |apartment|
+    user2.apartments.create(apartment)
+    p "created: #{apartment}"
+  end

9b3945a26fe0f453f96826f90bd6bf2d23e6da6c
Aaron Criner <aaron.criner@me.com>
2023-04-13 15:05:45 -0700

    added index listing and styling, finishing up testing

--- app/javascript/components/pages/ApartmentIndex.js
+++ app/javascript/components/pages/ApartmentIndex.js
@@ -1,11 +1,43 @@
-import React from 'react'
+import React from "react";
+import {
+  Card,
+  CardTitle,
+  CardBody,
+  CardSubtitle,
+  CardText,
+  Button,
+} from "reactstrap";
 
-const ApartmentIndex = () => {
+const ApartmentIndex = ({ apartments }) => {
   return (
     <>
-    ApartmentIndex
+      <h2>Current Listings</h2>
+      <div className="index-listing">
+        {apartments.map((apartment, index) => {
+          return (
+            <Card
+              key={index}
+              style={{
+                width: "18rem",
+              }}
+            >
+              <img alt="Sample" src={apartment.image} />
+              <CardBody>
+                <CardTitle tag="h5">{apartment.address}</CardTitle>
+                <CardSubtitle className="mb-2 text-muted" tag="h6">
+                  {apartment.city}, {apartment.state}
+                </CardSubtitle>
+                <CardText>
+                  {`This ${apartment.bedrooms} bedroom, ${apartment.bathrooms} bath apartment is BEGGING to get SLAMMED`}
+                </CardText>
+                <Button>Show More Info</Button>
+              </CardBody>
+            </Card>
+          );
+        })}
+      </div>
     </>
-  )
-}
+  );
+};
 
-export default ApartmentIndex
+export default ApartmentIndex;

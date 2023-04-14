3ec3511f979e724aa94e422b6f58f8858f8c9ef6
SDCurry3 <sncy23@gmail.com>
2023-04-14 12:50:01 -0700

    notfound page complete, jest testing not functional

--- app/javascript/components/pages/NotFound.js
+++ app/javascript/components/pages/NotFound.js
@@ -1,9 +1,16 @@
-import React from 'react'
+import React from 'react';
+import { Link } from 'react-router-dom';
+import BrokeStuff from '../../../assets/images/NotFound.JPG';
+import './NotFound.css'
 
 const NotFound = () => {
   return (
-    <div>NotFound</div>
-  )
+    <div className="not-found">
+      <img className="not-found-image" src={BrokeStuff} alt="Page not found" />
+      <br />
+      <Link to="/">Go back to home page</Link>
+    </div>
+  );
 }
 
-export default NotFound
+export default NotFound;

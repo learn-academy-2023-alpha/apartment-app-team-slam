7d504e3dc8d15f750bef0a6a64960189b55ebcdf
Aaron Criner <aaron.criner@me.com>
2023-04-14 11:03:56 -0700

    added test coverage and styling to apartment-show

--- app/javascript/components/pages/ApartmentShow.js
+++ app/javascript/components/pages/ApartmentShow.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React from "react";
 import {
   Card,
   CardTitle,
@@ -7,37 +7,53 @@ import {
   CardText,
   Button,
 } from "reactstrap";
-import {NavLink, useParams} from "react-router-dom";
+import { NavLink, useParams } from "react-router-dom";
 
-const ApartmentShow = ({apartments}) => {
-  let {id}=useParams()
-  const currentApartment=apartments?.find((apartment) => apartment.id === +id)
+const ApartmentShow = ({ apartments }) => {
+  let { id } = useParams();
+  const currentApartment = apartments?.find(
+    (apartment) => apartment.id === +id
+  );
 
   return (
     <>
-    <h2>Listing Info: </h2>
-    {currentApartment &&
-    <Card
-             className="show-card"
-              style={{
-                width: "18rem",
-              }}
-            >
-              <img alt="Sample" src={currentApartment.image} />
-              <CardBody>
-                <CardTitle tag="h5">{apartments.address}</CardTitle>
-                <CardSubtitle className="mb-2 text-muted" tag="h6">
-                  {currentApartment.city}, {currentApartment.state}
-                </CardSubtitle>
-                <CardText>
-                  {`This ${currentApartment.bedrooms} bedroom, ${currentApartment.bathrooms} bath apartment is BEGGING to get SLAMMED`}
-                </CardText>
-                <Button>
-                  <NavLink to={`/apartmentindex/`}>Back to listings</NavLink></Button>
-              </CardBody>
-            </Card>
-            }
-            </>)
-}
+      <h2>Listing Info: </h2>
+      <div className="apartment-show">
+        {currentApartment && (
+          <Card
+            className="show-card"
+            style={{
+              width: "18rem",
+            }}
+          >
+            <img alt="Sample" src={currentApartment.image} />
+            <CardBody className="card-show">
+              <CardTitle tag="h5">{apartments.address}</CardTitle>
+              <CardSubtitle className="mb-2 text-muted" tag="h6">
+                {currentApartment.city}, {currentApartment.state}
+              </CardSubtitle>
+              <CardText>
+                <h2>Price: ${currentApartment.price}</h2>
+                <table>
+                  <tr>
+                    <td>Bedrooms: {currentApartment.bedrooms}</td>
+                    <td>Bathrooms: {currentApartment.bathrooms}</td>
+                  </tr>
+                  <tr>
+                    <td>Square Footage: {currentApartment.square_footage}</td>
+                  </tr>
+                  <tr></tr>
+                </table>
+              </CardText>
+              <Button>
+                <NavLink to={`/apartmentindex/`}>Back to listings</NavLink>
+              </Button>
+            </CardBody>
+          </Card>
+        )}
+      </div>
+    </>
+  );
+};
 
-export default ApartmentShow
+export default ApartmentShow;
--- app/javascript/components/pages/ApartmentShow.test.js
+++ app/javascript/components/pages/ApartmentShow.test.js
@@ -6,12 +6,8 @@ import React from "react";
 import { isEqual } from "@rails/webpacker/package/utils/helpers.js";
 
 describe("<ApartmentShow />", () => {
-
   beforeEach(() => {
-
-    let {id}=1
-
-   
+    let { id } = 1;
 
     const current_user = {
       email: "test@testing.com",
@@ -34,7 +30,9 @@ describe("<ApartmentShow />", () => {
           "https://images.unsplash.com/photo-1522708323590-d24dbb6b0267?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80",
       },
     ];
-    const currentApartment=apartments?.find((apartment) => apartment.id === +id)
+    const currentApartment = apartments?.find(
+      (apartment) => apartment.id === +id
+    );
     render(
       <BrowserRouter>
         <ApartmentShow apartments={apartments} />
@@ -47,5 +45,4 @@ describe("<ApartmentShow />", () => {
   it("displays text current listings", () => {
     expect("Listing Info:").toBeInTheDocument;
   });
-
 });

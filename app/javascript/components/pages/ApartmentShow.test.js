b8a1319079f292568ec04e2d3f3d040fc80f5188
Aaron Criner <aaron.criner@me.com>
2023-04-14 11:25:30 -0700

    fixed logic placement

--- app/javascript/components/pages/ApartmentShow.test.js
+++ app/javascript/components/pages/ApartmentShow.test.js
@@ -30,9 +30,6 @@ describe("<ApartmentShow />", () => {
           "https://images.unsplash.com/photo-1522708323590-d24dbb6b0267?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80",
       },
     ];
-    const currentApartment = apartments?.find(
-      (apartment) => apartment.id === +id
-    );
     render(
       <BrowserRouter>
         <ApartmentShow apartments={apartments} />
